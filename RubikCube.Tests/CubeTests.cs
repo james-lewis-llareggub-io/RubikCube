5224bcc746c1dd300dbac8aa41e4d8a4727b569f
James Lewis <james.lewis@llareggub.io>
2023-02-24 12:23:13 +0000

    cube has an x, y and z location with a starting location of 0, 0, 0

---
+++ RubikCube.Tests/CubeTests.cs
@@ -0,0 +1,32 @@
+using System.Numerics;
+using FluentAssertions;
+
+namespace RubikCube.Tests;
+
+public class CubeTests
+{
+    private readonly Cube _cube;
+
+    public CubeTests()
+    {
+        _cube = new Cube();
+    }
+
+    [Fact]
+    public void Has_3D_initial_location_of_zero()
+    {
+        _cube.Location.X.Should().Be(0);
+        _cube.Location.Y.Should().Be(0);
+        _cube.Location.Z.Should().Be(0);
+    }
+}
+
+public class Cube
+{
+    public Cube()
+    {
+        Location = new Vector3();
+    }
+
+    public Vector3 Location { get; }
+}
