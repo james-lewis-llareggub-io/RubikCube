8db62ca874bdd60abd43627773d7cbec79a9b9e2
James Lewis <james.lewis@llareggub.io>
2023-02-25 08:29:05 +0000

    (pass-test) define a puzzle with 3x3x3 dimensions

--- RubikCube.Tests/PuzzleTests.cs
+++ RubikCube.Tests/PuzzleTests.cs
@@ -2,34 +2,41 @@
 
 public class PuzzleTests
 {
+    private readonly Puzzle _puzzle;
+
+    public PuzzleTests()
+    {
+        _puzzle = new Puzzle();
+    }
+
     [Fact]
     public void PuzzleHasWidthOfThree()
     {
-        var puzzle = new Puzzle();
-        puzzle.Width.Should().Be(3);
+        _puzzle.Width.Should().Be(3);
     }
 
     [Fact]
     public void PuzzleHasHeightOfThree()
     {
-        var puzzle = new Puzzle();
-        puzzle.Height.Should().Be(3);
+        _puzzle.Height.Should().Be(3);
     }
 
 
     [Fact]
     public void PuzzleHasDepthOfThree()
     {
-        var puzzle = new Puzzle();
-        puzzle.Depth.Should().Be(3);
+        _puzzle.Depth.Should().Be(3);
     }
 }
 
 public class Puzzle
 {
-    public int Width { get; set; }
+    public Puzzle()
+    {
+        Width = Height = Depth = 3;
+    }
 
-    public int Height { get; set; }
-
-    public int Depth { get; set; }
+    public int Width { get; }
+    public int Height { get; }
+    public int Depth { get; }
 }
