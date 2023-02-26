5fc09dbf34f46c15ce85a9517ba77be63538a53d
James Lewis <james.lewis@llareggub.io>
2023-02-26 08:20:54 +0000

    define 6 faces of puzzle Left, Right, Front, Back, Up, Down

---
+++ RubikCube.Tests/FaceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace RubikCube.Tests;
+
+public class FaceTests
+{
+    [Theory]
+    [InlineData(Face.Front, 'F')]
+    [InlineData(Face.Back, 'B')]
+    [InlineData(Face.Left, 'L')]
+    [InlineData(Face.Right, 'R')]
+    [InlineData(Face.Up, 'U')]
+    [InlineData(Face.Down, 'D')]
+    public void Face_keys_match(Face face, char character)
+    {
+        ((char)face).Should().Be(character);
+    }
+}
---
+++ RubikCube/Contracts/Face.cs
@@ -0,0 +1,11 @@
+﻿namespace RubikCube.Contracts;
+
+public enum Face
+{
+    Front = 'F',
+    Back = 'B',
+    Left = 'L',
+    Right = 'R',
+    Up = 'U',
+    Down = 'D'
+}
