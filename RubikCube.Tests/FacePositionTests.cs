5a3fd50e9bf4667eb1fd073baa6b1e65c41402d3
James Lewis <james.lewis@llareggub.io>
2023-02-26 10:36:36 +0000

    define face tag to store face and position 1-9 e.g. F1, F2, F3... F9

---
+++ RubikCube.Tests/FacePositionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RubikCube.Tests;
+
+public class FacePositionTests
+{
+    [Theory]
+    [InlineData(FacePosition.One, 1)]
+    [InlineData(FacePosition.Two, 2)]
+    [InlineData(FacePosition.Three, 3)]
+    [InlineData(FacePosition.Four, 4)]
+    [InlineData(FacePosition.Five, 5)]
+    [InlineData(FacePosition.Six, 6)]
+    [InlineData(FacePosition.Seven, 7)]
+    [InlineData(FacePosition.Eight, 8)]
+    [InlineData(FacePosition.Nine, 9)]
+    public void Face_keys_match(FacePosition position, int number)
+    {
+        ((int)position).Should().Be(number);
+    }
+}
---
+++ RubikCube/FaceTag.cs
@@ -0,0 +1,18 @@
+﻿namespace RubikCube;
+
+public readonly struct FaceTag
+{
+    public Face Face { get; }
+    public FacePosition Position { get; }
+
+    public FaceTag(Face face, FacePosition position)
+    {
+        Face = face;
+        Position = position;
+    }
+
+    public override string ToString()
+    {
+        return $"{(char)Face}{(int)Position}";
+    }
+}
