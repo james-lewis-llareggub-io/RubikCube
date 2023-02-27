454ecd500dc8ef87803ce19963aca0920c2c80fb
James Lewis <james.lewis@llareggub.io>
2023-02-27 10:04:40 +0000

    create a face tag holder to store and manage tags

---
+++ RubikCube/Contracts/IFaceTagHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace RubikCube.Contracts;
+
+public interface IFaceTagHolder
+{
+    IEnumerable<FaceTag> Tags { get; }
+    IFaceTagHolder Tag(FaceTag tag);
+    bool Exists(FaceTag tag);
+}
---
+++ RubikCube/FaceFaceTagHolder.cs
@@ -0,0 +1,27 @@
+﻿namespace RubikCube;
+
+public class FaceFaceTagHolder : IFaceTagHolder
+{
+    private readonly List<FaceTag> _tags;
+
+    public FaceFaceTagHolder()
+    {
+        _tags = new List<FaceTag>();
+    }
+
+    public IFaceTagHolder Tag(FaceTag tag)
+    {
+        if (Exists(tag))
+            _tags.Remove(tag);
+        _tags.Add(tag);
+
+        return this;
+    }
+
+    public bool Exists(FaceTag tag)
+    {
+        return _tags.Any(x => x.Face == tag.Face);
+    }
+
+    public IEnumerable<FaceTag> Tags => _tags;
+}
